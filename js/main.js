49da0a22ec015709ac9f8449bb3ca2c4bb54e0c8
onurkiris <83399747+onurkiris05@users.noreply.github.com>
2025-02-03 22:21:01 +0300

    [Added] Toggle Screen and Videos

--- js/main.js
+++ js/main.js
@@ -1,8 +1,43 @@
 const navBtn = document.querySelector(".menu");
 const navList = document.querySelector(".nav-list");
+const swiper = document.querySelector(".swiper");
+const content = document.querySelector(".content");
+const social = document.querySelector(".social-container");
+const bgVideo = document.querySelector(".bg-video-container");
+
+const joinUsButton = document.querySelector(".btn-join-us");
+const joinUsForm = document.querySelector(".join-us-container");
+const smallVideoContainer = document.querySelector(".small-video-container");
+const smallVideoButtons = document.querySelectorAll(".media-btn");
+const smallCards = document.querySelectorAll(".small-card");
+const smallVideo = document.querySelector(".small-video");
 
 navBtn.addEventListener("click", (e) => {
   e.preventDefault();
   navBtn.classList.toggle("active");
   navList.classList.toggle("active");
+});
+
+function toggleJoinUs() {
+  swiper.classList.toggle("hidden");
+  content.classList.toggle("hidden");
+  social.classList.toggle("hidden");
+  bgVideo.classList.toggle("hidden");
+
+  joinUsForm.classList.toggle("hidden");
+  smallVideo.classList.toggle("hidden");
+  smallCards.forEach((card) => {
+    card.classList.toggle("hidden");
+  });
+  smallVideoButtons.forEach((btn) => {
+    btn.classList.toggle("hidden");
+  });
+
+  smallVideoContainer.dataset.box =
+    smallVideoContainer.dataset.box === "active" ? "hidden" : "active";
+}
+
+joinUsButton.addEventListener("click", toggleJoinUs);
+smallVideoButtons.forEach((btn) => {
+  btn.addEventListener("click", toggleJoinUs);
 });
--- js/swiper.js
+++ js/swiper.js
@@ -9,6 +9,9 @@ const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "vertical",
   loop: true,
+  autoplay: {
+    delay: 5000,
+  },
 
   // If we need pagination
   pagination: {
